b53e845c13c483a5b6612375f0981e306c486ad3
insanetim <47120724+insanetim@users.noreply.github.com>
2023-04-09 16:30:45 +0300

    added _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
+import Head from 'next/head'
 import { AppProps } from 'next/dist/shared/lib/router/router'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link
+          rel='icon'
+          href='/favicon.ico'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home(): JSX.Element {
@@ -6,14 +5,6 @@ export default function Home(): JSX.Element {
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link
-          rel='icon'
-          href='/favicon.ico'
-        />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
