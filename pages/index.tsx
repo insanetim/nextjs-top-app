5286c66fa512251bd353a1db35c3605c5a57ae39
insanetim <47120724+insanetim@users.noreply.github.com>
2023-04-11 17:06:34 +0300

    added courses

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
+import { GetStaticProps } from 'next'
+import axios from 'axios'
 
 import { Htag, Rating } from 'components'
 import { withLayout } from 'layout/Layout'
+import { MenuItem } from 'interfaces/menu.interface'
 
-function Home() {
+function Home({ menu }: HomeProps) {
   const [rating, setRating] = useState(3)
 
   return (
@@ -14,8 +17,32 @@ function Home() {
         isEditable
         setRating={setRating}
       />
+      <ul>
+        {menu.map(m => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   )
 }
 
 export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/top-page/find', {
+    firstCategory
+  })
+
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[]
+  firstCategory: number
+}
